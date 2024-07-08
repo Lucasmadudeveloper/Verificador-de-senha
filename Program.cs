dc99ba709f8e6a981f8d1a19b4b517f883393101
s-Lucas.CSouza <s-Lucas.CSouza@BIWL0538>
2024-07-08 10:52:13 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;  // Importa o namespace System, que contém classes fundamentais para o .NET, como Console.
+
+namespace Aula_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "123";  // Define a senha correta como "123".
+            string senhauser;  // Declara uma variável para armazenar a senha digitada pelo usuário.
+            int tentativas = 0;  // Inicializa o contador de tentativas com 0.
+
+            do
+            {
+                Console.Clear();  // Limpa a tela do console.
+                Console.WriteLine("Digite a senha");  // Exibe a mensagem solicitando que o usuário digite a senha.
+                senhauser = Console.ReadLine();  // Lê a senha digitada pelo usuário e a armazena na variável 'senhauser'.
+                tentativas++;  // Incrementa o contador de tentativas.
+
+                if (senha != senhauser)  // Verifica se a senha digitada pelo usuário está incorreta.
+                {
+                    Console.WriteLine("Senha errada, tente novamente");  // Exibe a mensagem de senha incorreta.
+                    System.Threading.Thread.Sleep(2000);  // Pausa a execução por 2 segundos para que o usuário possa ver a mensagem.
+                }
+
+            } while (senha != senhauser);  // Continua o loop enquanto a senha digitada pelo usuário for incorreta.
+
+            Console.WriteLine($"Senha correta, tentativas: {tentativas}");  // Exibe uma mensagem indicando que a senha correta foi digitada e mostra o número de tentativas.
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Aula_21")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Vallourec")]
+[assembly: AssemblyProduct("Aula_21")]
+[assembly: AssemblyCopyright("Copyright © Vallourec 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("1c62698f-8066-4309-86e1-d986c60be1d2")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
